e2aff84bc997f4506286ccf013504a9dfa9676a3
Farhan Hasan <hasanfarhan33@gmail.com>
2021-08-29 21:58:52 +0300

    Added more contacts

--- automatedWhatsApp.py
+++ automatedWhatsApp.py
@@ -1,5 +1,6 @@
 import pywhatkit as pwk
 # pwk.sendwhatmsg("+971 54 353 8848", "This is an automated message, we are watching you.", 19, 5)     #AUTOMATED MESSAGE TO MIRZA
 # pwk.sendwhatmsg("+90 539 936 05 11", "Give me food, I am hungry.", 19, 20)  #AUTOMATED MESSAGE TO AMMA
-pwk.sendwhatmsg("+90 538 414 89 70", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 27) #AUTOMATED MESSAGE TO ENVER
-pwk.sendwhatmsg("+90 543 676 04 95", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 29)
+# pwk.sendwhatmsg("+90 538 414 89 70", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 27) #AUTOMATED MESSAGE TO ENVER
+# pwk.sendwhatmsg("+90 543 676 04 95", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 29) #AUTOMATED MESSAGE TO BARIS
+pwk.sendwhatmsg("+90 539 853 18 52", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number.", 19, 40) #AUTOMATED MESSAGE TO EGE

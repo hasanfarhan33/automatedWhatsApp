fcf637998d52e1bdc9c3a2e3241b25d9b1a0bfbe
Farhan Hasan <hasanfarhan33@gmail.com>
2021-08-29 19:30:16 +0300

    Tested sending messages to multiple people. Worked like a charm.

---
+++ automatedWhatsApp.py
@@ -0,0 +1,5 @@
+import pywhatkit as pwk
+# pwk.sendwhatmsg("+971 54 353 8848", "This is an automated message, we are watching you.", 19, 5)     #AUTOMATED MESSAGE TO MIRZA
+# pwk.sendwhatmsg("+90 539 936 05 11", "Give me food, I am hungry.", 19, 20)  #AUTOMATED MESSAGE TO AMMA
+pwk.sendwhatmsg("+90 538 414 89 70", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 27) #AUTOMATED MESSAGE TO ENVER
+pwk.sendwhatmsg("+90 543 676 04 95", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 29)

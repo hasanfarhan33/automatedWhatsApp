1daff9bb12069152918e8d860257fc08e979e2fe
Farhan Hasan <hasanfarhan33@gmail.com>
2021-08-30 23:14:11 +0300

    Added Nilufer contact

--- automatedWhatsApp.py
+++ automatedWhatsApp.py
@@ -3,4 +3,6 @@ import pywhatkit as pwk
 # pwk.sendwhatmsg("+90 539 936 05 11", "Give me food, I am hungry.", 19, 20)  #AUTOMATED MESSAGE TO AMMA
 # pwk.sendwhatmsg("+90 538 414 89 70", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 27) #AUTOMATED MESSAGE TO ENVER
 # pwk.sendwhatmsg("+90 543 676 04 95", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number", 19, 29) #AUTOMATED MESSAGE TO BARIS
-pwk.sendwhatmsg("+90 539 853 18 52", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number.", 19, 40) #AUTOMATED MESSAGE TO EGE
+# pwk.sendwhatmsg("+90 539 853 18 52", "Don't freak out, this is just an automated message. I am testing something on Python. Sorry to use your number.", 19, 40) #AUTOMATED MESSAGE TO EGE
+# pwk.sendwhatmsg("+90 538 397 78 28", "This is an automated message. If you want the script let me know. You can also scare someone.", 21, 45) #AUTOMATED MESSAGE TO NILUFER
+
